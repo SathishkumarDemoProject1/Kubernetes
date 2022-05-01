ec5d6376d7bc5fa6075f82dd638166c7aebe7092
SathishkumarDemoProject1 <43246727+SathishkumarDemoProject1@users.noreply.github.com>
2022-05-01 22:03:47 +0530

    Update start.sh

--- giftingapp-merchant-service-5731e4266c57/start.sh
+++ giftingapp-merchant-service-5731e4266c57/start.sh
@@ -2,6 +2,6 @@ mvn clean install
 
 docker login -u $USER_NAME -p $PASSWORD
 
-docker build ./ -t giftingapp-merchant-service/merchant-service:0.0.1
+docker build ./ -t sathishkumar281995/giftingapp-merchant-service:latest
 
-docker push giftingapp-merchant-service/merchant-service:0.0.1
+docker push sathishkumar281995/giftingapp-merchant-service:latest
